1818a57a8303bfbef52b300d2ee86bfac93d0b36
woals <woals6318@gmail.com>
2026-04-28 09:21:19 +0900

    add user_info.dart file

---
+++ lib/models/user_info.dart
@@ -0,0 +1,27 @@
+class UserInfo {
+  String? name;
+  String? email;
+  String? profileImagePath;
+
+  UserInfo({
+    this.name,
+    this.email,
+    this.profileImagePath
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'profileImagePath': profileImagePath
+    };
+  }
+
+  factory UserInfo.fromMap(Map<String, dynamic> userInfoMap) {
+    return UserInfo(
+      name: userInfoMap['name'],
+      email: userInfoMap['email'],
+      profileImagePath: userInfoMap['profileImagePath'],
+    );
+  }
+}
